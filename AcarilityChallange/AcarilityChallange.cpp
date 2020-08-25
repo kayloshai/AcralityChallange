8b1c58823d00f3a9370ed92eb4761e88bda52702
Jason Lavarinhas <kayloshai@gmail.com>
2020-08-25 09:33:30 +0100

    Initial Upload

---
+++ AcarilityChallange/AcarilityChallange.cpp
@@ -0,0 +1,17 @@
+// AcarilityChallange.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+class Acrality {
+public:
+	Acrality() {
+		std::cout << "Acrality Programming Challange.";
+	}
+};
+
+int main()
+{
+	Acrality acrObj;
+    std::cout << "Hello World!\n";
+}
